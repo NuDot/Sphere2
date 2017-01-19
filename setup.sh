e3aebb60e67dbdccda3528f97a843f44a90919bb
Suzannah Fraker <sfraker@nunet.lns.mit.edu>
2017-01-19 16:23:04 -0500

    Add setup script for nudot.lns.mit.edu.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /home/taritree/software/geant4.10.03/release/share/Geant4-10.3.0/geant4make/geant4make.sh 
+source /home/sfraker/setup_geant.sh 
+export G4WORKDIR=/home/sfraker/g4w
+
