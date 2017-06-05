7fa5e9700b77913ad9d08956d60703da19f88daf
Mikaeel Yunus <myunus@nunet.lns.mit.edu>
2017-06-05 16:34:52 -0400

    Just a little bit of cleanup.

--- src/Sphere1PrimaryGeneratorAction.cc
+++ src/Sphere1PrimaryGeneratorAction.cc
@@ -1,4 +1,32 @@
-#include <cmath>
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file Sphere1PrimaryGeneratorAction.cc
+/// \brief Implementation of the Sphere1PrimaryGeneratorAction class
 
 #include "Sphere1PrimaryGeneratorAction.hh"
 
@@ -13,6 +41,8 @@
 #include "Randomize.hh"
 #include "G4Event.hh"
 #include "G4IonTable.hh"
+
+#include <cmath>
 
 const int USE_HEPEVT_INTERFACE = 0;
 const char* HEPEVT_FILE = "/mnt/disk0/kamland/spherical_data/C10_prompt_0p79MeV_pos_0p718MeV_gamma_1k.EVT";
